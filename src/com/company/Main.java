79371d1935d24ccf1c4407fd4f5711f738d228cd
Olha Shpytalna <olga.shpytalna@gmail.com>
2017-11-12 09:56:47 +0200

    Updated sout messages

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,5 +6,8 @@ public class Main {
 	// write your code here
         System.out.println("Hi");
         System.out.println("have a good day");
+        System.out.println("hi again");
+
+
     }
 }
